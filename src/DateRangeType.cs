a78bc9ab953774f69608be39b5e3469bd71252a7
Jake Soenneker <jake@soenneker.com>
2025-02-25 08:47:00 -0600

    Initial commit

---
+++ src/DateRangeType.cs
@@ -0,0 +1,28 @@
+﻿using Intellenum;
+
+namespace Soenneker.Enums.DateRangeType;
+
+/// <summary>
+/// An enumerator for describing common date ranges
+/// </summary>
+[Intellenum<string>]
+public partial class DateRangeType
+{
+    public static readonly DateRangeType Today = new(nameof(Today));
+
+    public static readonly DateRangeType Yesterday = new(nameof(Yesterday));
+
+    public static readonly DateRangeType CurrentWeek = new(nameof(CurrentWeek));
+
+    public static readonly DateRangeType PreviousWeek = new(nameof(PreviousWeek));
+
+    public static readonly DateRangeType CurrentMonth = new(nameof(CurrentMonth));
+
+    public static readonly DateRangeType PreviousMonth = new(nameof(PreviousMonth));
+
+    public static readonly DateRangeType CurrentYear = new(nameof(CurrentYear));
+
+    public static readonly DateRangeType PreviousYear = new(nameof(PreviousYear));
+
+    public static readonly DateRangeType Custom = new(nameof(Custom));
+}
---
+++ test/Soenneker.Enums.DateRangeType.Tests/DateRangeTypeTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Enums.DateRangeType.Tests;
+
+[Collection("Collection")]
+public class DateRangeTypeTests : FixturedUnitTest
+{
+    public DateRangeTypeTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
