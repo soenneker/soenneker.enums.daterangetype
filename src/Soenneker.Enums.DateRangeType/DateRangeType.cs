b8b73ec7e856b30feaf89869b55ad50c12af0ae5
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:29 -0500

    xml commenting

--- src/Soenneker.Enums.DateRangeType/DateRangeType.cs
+++ src/Soenneker.Enums.DateRangeType/DateRangeType.cs
@@ -10,21 +10,48 @@ namespace Soenneker.Enums.DateRangeType;
 [EnumValue<string>]
 public sealed partial class DateRangeType
 {
+    /// <summary>
+    /// The today.
+    /// </summary>
     public static readonly DateRangeType Today = new(nameof(Today));
 
+    /// <summary>
+    /// The yesterday.
+    /// </summary>
     public static readonly DateRangeType Yesterday = new(nameof(Yesterday));
 
+    /// <summary>
+    /// The current week.
+    /// </summary>
     public static readonly DateRangeType CurrentWeek = new(nameof(CurrentWeek));
 
+    /// <summary>
+    /// The previous week.
+    /// </summary>
     public static readonly DateRangeType PreviousWeek = new(nameof(PreviousWeek));
 
+    /// <summary>
+    /// The current month.
+    /// </summary>
     public static readonly DateRangeType CurrentMonth = new(nameof(CurrentMonth));
 
+    /// <summary>
+    /// The previous month.
+    /// </summary>
     public static readonly DateRangeType PreviousMonth = new(nameof(PreviousMonth));
 
+    /// <summary>
+    /// The current year.
+    /// </summary>
     public static readonly DateRangeType CurrentYear = new(nameof(CurrentYear));
 
+    /// <summary>
+    /// The previous year.
+    /// </summary>
     public static readonly DateRangeType PreviousYear = new(nameof(PreviousYear));
 
+    /// <summary>
+    /// The custom.
+    /// </summary>
     public static readonly DateRangeType Custom = new(nameof(Custom));
 }
